b5399e709a868f8c98bd2c6e478e7fef8451d1d2
maheshwari522 <maheshwaribhandare2205@gmail.com>
2025-02-14 18:22:51 -0800

    Time: 1 ms (100%), Space: 42.7 MB (38.38%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,75 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+
+
+        Queue<int[]> q =new LinkedList<>();
+
+       
+
+        int rowlength = grid.length;
+        int collength = grid[0].length;
+
+
+        int fresh = 0;
+    
+
+        for(int i = 0;i<rowlength;i++){
+            for(int j = 0 ; j<collength ; j++){
+                if(grid[i][j] == 1){
+                    fresh++;
+                }else if(grid[i][j] == 2){
+                   
+                    q.add(new int[]{i,j,0});
+                }
+            }
+        }
+
+      
+
+        int[] dx = {-1,0,1,0};
+        int[] dy = {0,1,0,-1};
+        int maxTime = 0;
+
+        while(!q.isEmpty()){
+
+           
+                int[] current = q.poll();
+
+                int row = current[0];
+                int col = current[1];
+                int time = current[2];
+
+                maxTime = Math.max(time, maxTime);
+
+                for(int i = 0; i< 4;i++){
+                    int nextrow = row+dx[i];
+                    int nextcol = col+dy[i];
+
+                    if(nextrow>=0 && nextrow < rowlength && nextcol >=0 && nextcol < collength && grid[nextrow][nextcol]== 1){
+                        grid[nextrow][nextcol] = 2;
+
+                        fresh--;
+
+                        q.add(new int[]{nextrow,nextcol,time+1});
+                    }
+                }
+
+
+
+
+            
+
+
+
+
+        }
+
+
+
+        return fresh == 0? maxTime : -1;
+
+
+        
+    }
+
+}
