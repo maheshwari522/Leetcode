293a1a9c814561b24adb7c7a860ebf6f7474d242
maheshwari522 <maheshwaribhandare2205@gmail.com>
2025-03-24 12:57:54 -0700

    Time: 4 ms (50.55%), Space: 49.8 MB (45.46%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,40 +1,44 @@
 class Solution {
     public int numIslands(char[][] grid) {
 
-        if(grid == null || grid.length == 0){
-            return 0;
-        }
+        //using dfs because one iland will be traveled to it's depth
 
-        int numOfIslands = 0;
+        // checking all 4 directions
 
-        for(int i =0;i<grid.length;i++){
-            for(int j = 0;j < grid[0].length;j++){
-                if(grid[i][j] == '1'){
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int count = 0;
+        boolean[][] visited = new boolean[rows][cols];
 
-                    numOfIslands++;
-
-                    dfs(grid,i,j);
-
+        for(int i = 0;i< rows;i++){
+            for(int j = 0;j<cols ;j++){
+                if(grid[i][j] == '1' && visited[i][j] != true){
+                    
+                    
+                    startDFS(grid,visited,i,j,rows,cols);
+                    count++;
                 }
             }
         }
 
-        return numOfIslands;
-
+    return count;
+        
+        
     }
 
-    public void dfs(char[][] grid, int i, int j){
+    public void startDFS(char[][] grid,boolean[][] visited,int i,int j,int rows, int cols){
+            
+            if(i<0 || i>= rows || j<0 || j>=cols ||  visited[i][j] == true) return;
+           
+            if(grid[i][j] == '1'){
+            visited[i][j] = true;
+            startDFS(grid,visited,i+1,j,rows,cols);
+            startDFS(grid,visited,i-1,j,rows,cols);
+            startDFS(grid,visited,i,j+1,rows,cols);
+            startDFS(grid,visited,i,j-1,rows,cols);
+            }
 
-
-        if(i>=grid.length || j>=grid[0].length || i< 0 || j< 0 || grid[i][j] != '1'){
-            return;
+            
+            
         }
-        grid[i][j] = '0';
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
-
-    }
-
 }
