8794a8b1a39b2ce4ceb207c6e254489d208ae9cc
maheshwari522 <maheshwaribhandare2205@gmail.com>
2025-03-24 13:03:53 -0700

    Time: 3 ms (86.32%), Space: 50.3 MB (27.47%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -8,14 +8,14 @@ class Solution {
         int rows = grid.length;
         int cols = grid[0].length;
         int count = 0;
-        boolean[][] visited = new boolean[rows][cols];
+      
 
         for(int i = 0;i< rows;i++){
             for(int j = 0;j<cols ;j++){
-                if(grid[i][j] == '1' && visited[i][j] != true){
+                if(grid[i][j] == '1'){
                     
                     
-                    startDFS(grid,visited,i,j,rows,cols);
+                    startDFS(grid,i,j,rows,cols);
                     count++;
                 }
             }
@@ -26,16 +26,16 @@ class Solution {
         
     }
 
-    public void startDFS(char[][] grid,boolean[][] visited,int i,int j,int rows, int cols){
+    public void startDFS(char[][] grid,int i,int j,int rows, int cols){
             
-            if(i<0 || i>= rows || j<0 || j>=cols ||  visited[i][j] == true) return;
+            if(i<0 || i>= rows || j<0 || j>=cols ||  grid[i][j] == '0') return;
            
             if(grid[i][j] == '1'){
-            visited[i][j] = true;
-            startDFS(grid,visited,i+1,j,rows,cols);
-            startDFS(grid,visited,i-1,j,rows,cols);
-            startDFS(grid,visited,i,j+1,rows,cols);
-            startDFS(grid,visited,i,j-1,rows,cols);
+            grid[i][j] = '0';
+            startDFS(grid,i+1,j,rows,cols);
+            startDFS(grid,i-1,j,rows,cols);
+            startDFS(grid,i,j+1,rows,cols);
+            startDFS(grid,i,j-1,rows,cols);
             }
 
             
