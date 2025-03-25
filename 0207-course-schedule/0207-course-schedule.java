1fbccd5fcb3b0243cddbde5291d3121d3e5186d7
maheshwari522 <maheshwaribhandare2205@gmail.com>
2025-03-25 12:49:29 -0700

    Time: 7 ms (51.66%), Space: 45.8 MB (30.29%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,34 +1,37 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-
+        //adjacanecy matrix
         List<List<Integer>> adj = new ArrayList<>();
-
+        //add elements
         for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
-        int[] indegree = new int[numCourses];
 
+        //indegree count array
+        int[] indegree = new int[numCourses];
+       //add elements to adjacency list
         for(int[] prereq : prerequisites){
            adj.get(prereq[1]).add(prereq[0]);
            
         }
-
+        //idegree calculation
         for(int i = 0;i<indegree.length;i++){
             for(int j : adj.get(i)){
                 indegree[j]++;
             }
         }
 
+       ///topo sort code
         Queue<Integer> q = new LinkedList<>();
-
+        // adding to q whose indewgree is 0
         for(int i=0;i<numCourses;i++){
             if(indegree[i] == 0){
                 q.offer(i);
             }
         }
-
+        //topo list
         List<Integer> topo = new ArrayList<>();
-
+    
         while(!q.isEmpty()){
             int node = q.peek();
             q.remove();
