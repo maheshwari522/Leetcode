666c7a3c9853578012f3d1883039721d4cd98b17
Maheshwari Bhandare <maheshwaribhandare2205@gmail.com>
2025-06-25 11:23:49 -0700

    Time: 7 ms (74.24%), Space: 43 MB (51.18%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,46 +1,40 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
 
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
 
         if(s1.length()>s2.length()) return false;
 
-        int[] s1count = new int[26];
-        int[] s2count = new int[26];
-
         for(int i=0;i<s1.length();i++){
-            s1count[s1.charAt(i) - 'a']++;
-            s2count[s2.charAt(i) - 'a']++;
+            s1Count[s1.charAt(i)-'a']++;
+            s2Count[s2.charAt(i)-'a']++;
+
+
         }
 
-        if(matches(s1count,s2count)) return true;
+        if(matches(s1Count,s2Count)) return true;
 
-        for(int i =s1.length();i<s2.length();i++){
+        int left = 0;
+        
 
-            System.out.println(s2.charAt(i));
-            s2count[s2.charAt(i) - 'a']++;
-             System.out.println(s2.charAt(i - s1.length()));
-
-            s2count[s2.charAt(i-s1.length()) - 'a' ]--;
-            if(matches(s1count,s2count)) return true;
+        for(int right = s1.length();right<s2.length();right++){
+            s2Count[s2.charAt(right)-'a']++;
+            s2Count[s2.charAt(left)-'a']--;
+            left++;
+            if(matches(s1Count,s2Count)) return true;
         }
 
         return false;
-
-        
         
     }
 
-
-    public boolean matches(int[] s1count,int[] s2count){
-
-        for(int i = 0;i<26;i++){
-            if(s1count[i] != s2count[i]){
+    public boolean matches(int[] s1Count, int[] s2Count){
+        for(int i=0;i<26;i++){
+            if(s1Count[i]!=s2Count[i]){
                 return false;
             }
         }
-
-            return true;
-        
+        return true;
     }
-    
 }
