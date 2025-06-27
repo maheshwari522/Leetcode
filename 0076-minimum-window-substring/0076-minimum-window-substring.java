3c7fbc4569d529902802d20b243789ac1a16b3e2
Maheshwari Bhandare <maheshwaribhandare2205@gmail.com>
2025-06-27 14:36:28 -0700

    Time: 17 ms (63.09%), Space: 44.8 MB (73.75%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,60 +1,42 @@
 class Solution {
     public String minWindow(String s, String t) {
+    int left = 0;
+    HashMap<Character,Integer> tmap = new HashMap<>();
+    for(char c:t.toCharArray()){
+        tmap.put(c, tmap.getOrDefault(c,0)+1);
+    }
+    int requiredChars = t.length();
+    int minstart = 0;
 
-        int start = 0;
-        
+    int minLength=Integer.MAX_VALUE;
 
-        HashMap<Character,Integer> map = new HashMap<>();
+    for(int right = 0; right<s.length();right++){
+        char cright = s.charAt(right);
+        if(tmap.containsKey(cright)){
+            if(tmap.get(cright)>0)
+            requiredChars--;
+            tmap.put(cright, tmap.get(cright)-1);
+        }
+        while(requiredChars == 0){
+            if(right-left+1 < minLength){
+                minLength = right-left+1;
+                minstart = left;
+            }
 
-        for(char c:t.toCharArray()){
+            char cleft = s.charAt(left);
 
-         map.put(c,map.getOrDefault(c,0)+1);
+            if(tmap.containsKey(cleft)){
+               tmap.put(cleft,tmap.get(cleft)+1); 
+               if(tmap.get(cleft)>0)
+               requiredChars++;
+            }
+
+            left++;
+
 
         }
+    }
 
-     
-        int minLength = Integer.MAX_VALUE;
-
-        int minStart = 0;
-
-        int requiredchars = t.length();
-
-        
-
-        for(int end = 0;end< s.length();end++){
-
-            char ch = s.charAt(end);
-            if(map.containsKey(ch)){
-                
-                if(map.get(ch)>0){
-
-                   requiredchars--;
-                    
-                }
-                map.put(ch,map.get(ch)-1);
-                }
-
-                while(requiredchars == 0){
-                    if((end-start+1) < minLength){
-                        minLength = end-start+1;
-                        minStart = start;
-                     }
-
-                     char cstart = s.charAt(start);
-
-                     if(map.containsKey(cstart)){
-                        map.put(cstart,map.get(cstart)+1);
-                        if(map.get(cstart)>0)requiredchars++;
-                     }
-
-                     start++;
-                }
-        }
-
-       
-
-       String res = minLength == Integer.MAX_VALUE ? "" : s.substring(minStart,minStart+minLength);
-
-       return res;
+    return minLength == Integer.MAX_VALUE? "" : s.substring(minstart,minstart+minLength);
     }
 }
