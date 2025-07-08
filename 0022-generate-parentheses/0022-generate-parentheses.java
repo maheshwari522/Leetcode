d196ee983f8f1f018aa30c51f045a6b9bad9c205
Maheshwari Bhandare <maheshwaribhandare2205@gmail.com>
2025-07-08 16:22:39 -0700

    Time: 1 ms (82.79%), Space: 42.7 MB (94.47%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,40 +1,36 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
 
-        List<String> res = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
+        List<String> ans = new ArrayList<>();
 
-        backtrack(0,0,n,res,sb);
+        StringBuilder s = new StringBuilder();
 
+        helper(0,0,n,s, ans);
 
-        return res;
+        return ans;
         
-
-
     }
 
-    public void backtrack(int openN, int closeN, int n, List<String> res, StringBuilder sb){
+    public void helper(int openn, int closen, int n,StringBuilder s,List<String> ans){
+        if(openn == closen && openn == n){
+            ans.add(s.toString());
+            return;
+        }
 
-    if(openN == closeN && openN == n){
-        res.add(sb.toString());
-        return;
+        if(openn<n){
+            s.append("(");
+            helper(openn+1,closen,n,s,ans);
+            s.deleteCharAt(s.length() -1);
+        }
 
-    }
-
-    if(openN<n){
-        sb.append('(');
-        backtrack(openN+1,closeN,n,res,sb);
-        sb.deleteCharAt(sb.length() -1);
-    }
-
-    if(closeN<openN){
-        sb.append(')');
-        backtrack(openN,closeN+1,n,res,sb);
-        sb.deleteCharAt(sb.length() -1 );
-    }
+        if(closen<openn){
+            s.append(")");
+            helper(openn,closen+1,n,s,ans);
+            s.deleteCharAt(s.length() -1);
 
 
+        }
 
-
+        
     }
 }
