7337a2118d42eb5d3a76b340e975c219c6508cf3
Maheshwari Bhandare <maheshwaribhandare2205@gmail.com>
2025-08-01 14:38:57 -0700

    Time: 22 ms (55.22%), Space: 45.6 MB (74.01%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,36 +1,42 @@
-public class Solution {
+class Solution {
     public int leastInterval(char[] tasks, int n) {
+
         int[] count = new int[26];
-        for (char task : tasks) {
-            count[task - 'A']++;
+
+        for(char c:tasks){
+            count[c - 'A']++;
         }
 
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        for (int cnt : count) {
-            if (cnt > 0) {
-                maxHeap.add(cnt);
+       int time = 0;
+
+       PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i = 0;i<26;i++){
+            if(count[i]>0){
+                q.add(count[i]);
             }
         }
 
-        int time = 0;
-        Queue<int[]> q = new LinkedList<>();
-        while (!maxHeap.isEmpty() || !q.isEmpty()) {
+        Queue<int[]> temptask = new LinkedList<>();
+
+        while(!q.isEmpty()||!temptask.isEmpty()){
             time++;
-            
-            if (maxHeap.isEmpty()) {
-                time = q.peek()[1];
-            } else {
-                int cnt = maxHeap.poll() - 1;
-                if (cnt > 0) {
-                    q.add(new int[]{cnt, time + n});
+            if(q.isEmpty()){
+                 time = temptask.peek()[1];
+
+            }else{
+                int currentfreq = q.poll()-1;
+                if(currentfreq>0){
+                temptask.add(new int[]{currentfreq,time+n});
                 }
             }
 
-            if (!q.isEmpty() && q.peek()[1] == time) {
-                maxHeap.add(q.poll()[0]);
+            if(!temptask.isEmpty() && temptask.peek()[1] == time){
+                q.add(temptask.poll()[0]);
             }
         }
-        
+
         return time;
+        
     }
 }
