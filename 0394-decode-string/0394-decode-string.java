eb335444daf904c1c7c0db1c964f73e428cbd605
Maheshwari Bhandare <maheshwaribhandare2205@gmail.com>
2025-08-15 13:31:45 -0700

    Time: 3 ms (20.82%), Space: 41.9 MB (19.99%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,48 +1,42 @@
 class Solution {
     public String decodeString(String s) {
+        
 
         Stack<Character> st = new Stack<>();
 
-        for(char c:s.toCharArray()){
-            if(c != (']')){
+        for(char c: s.toCharArray()){
+
+            if(c != ']'){
                 st.push(c);
-            
             }else{
                 StringBuilder sb = new StringBuilder();
-                while(st.peek() != '['){
+                while(st.peek()!='['){
                     sb.insert(0,st.pop());
-                }
-                String sub = sb.toString();
-                st.pop();
 
+                }
+                String substr = sb.toString();
+                st.pop();
                 sb = new StringBuilder();
                 while(!st.isEmpty() && Character.isDigit(st.peek())){
                     sb.insert(0,st.pop());
                 }
-
                 int count = Integer.valueOf(sb.toString());
-
-                while(count > 0){
-                    for(char ch:sub.toCharArray()){
-                        st.push(ch);
-                    }
-
+                while(count>0){
+                    for(char ch:substr.toCharArray()) st.push(ch);
+                    
                     count--;
-
                 }
-
 
             }
         }
 
         StringBuilder res = new StringBuilder();
-
         while(!st.isEmpty()){
-            res.insert(0,st.pop());
-
+            res.append(st.pop());
         }
 
-        return res.toString();
+        return res.reverse().toString();
+
         
     }
 }
