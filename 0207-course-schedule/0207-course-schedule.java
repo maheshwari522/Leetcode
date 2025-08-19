b7d246601f6200cc0c0ae332599cf632b69dff75
Maheshwari Bhandare <maheshwaribhandare2205@gmail.com>
2025-08-18 18:09:52 -0700

    Time: 6 ms (72.87%), Space: 45.1 MB (83.34%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,54 +1,58 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        //adjacanecy matrix
-        List<List<Integer>> adj = new ArrayList<>();
-        //add elements
-        for(int i=0;i<numCourses;i++){
-            adj.add(new ArrayList<>());
-        }
 
-        //indegree count array
+
+        //create adjacescy matrix
+
+        List<List<Integer>> adj = new ArrayList();
+
+        //create indegree no of edges incoming
+
         int[] indegree = new int[numCourses];
-       //add elements to adjacency list
-        for(int[] prereq : prerequisites){
-           adj.get(prereq[1]).add(prereq[0]);
-           
-        }
-        //idegree calculation
-        for(int i = 0;i<indegree.length;i++){
-            for(int j : adj.get(i)){
-                indegree[j]++;
-            }
+
+        for(int i =0;i< numCourses;i++){
+            adj.add(new ArrayList());
+
+
         }
 
-       ///topo sort code
+
+        for(int[] preq : prerequisites){
+            int course = preq[0];
+            int prereq = preq[1];
+
+            adj.get(prereq).add(course);
+            indegree[course]++;
+
+
+        }
+
+
         Queue<Integer> q = new LinkedList<>();
-        // adding to q whose indewgree is 0
-        for(int i=0;i<numCourses;i++){
+
+        for(int i = 0;i<numCourses;i++){
             if(indegree[i] == 0){
                 q.offer(i);
             }
         }
-        //topo list
-        List<Integer> topo = new ArrayList<>();
-    
+
+        List<Integer> ans = new ArrayList<>();
+
         while(!q.isEmpty()){
-            int node = q.peek();
-            q.remove();
+            int current = q.poll();
+            ans.add(current);
 
-            topo.add(node);
+            for(int n :adj.get(current)){
+                indegree[n]--;
 
-            for (int it : adj.get(node)) {
-                indegree[it]--;
-                if (indegree[it] == 0) q.add(it);
+                if(indegree[n] == 0){
+                    q.offer(n);
+                }
             }
         }
 
-       if(topo.size() == numCourses) return true;
-
-       return false;
 
 
-        
+        return ans.size() == numCourses;
     }
 }
