9cbd4fb121cd2d3696d1e80bc85d501754c23972
Maheshwari Bhandare <maheshwaribhandare2205@gmail.com>
2025-08-21 21:42:03 -0700

    Time: 2 ms (93.83%), Space: 56.4 MB (11.31%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+
+        int five = 0;
+        int ten = 0;
+        int twenty = 0;
+        int res = 0;
+
+        for(int i:bills){
+            if(i == 5){
+                five++;
+            }else if( i == 10){
+                if(five == 0) return false;
+               
+                five--;
+                ten++;
+                
+            }else{
+                twenty++;
+                if(ten>0 && five>0){
+                    ten--;
+                    five--;
+                    
+                }
+                else if(five>=3){
+                    five=five-3;
+                  
+                }else{
+                    return false;
+                }
+            }
+
+        }
+
+        
+        return true;
+        
+    }
+}
