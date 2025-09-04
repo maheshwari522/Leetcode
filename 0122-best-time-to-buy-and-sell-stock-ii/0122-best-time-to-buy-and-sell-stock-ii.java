95f8eeb2f9e8eee9d2e4f014f203a3617c09b041
Maheshwari Bhandare <maheshwaribhandare2205@gmail.com>
2025-09-04 15:48:32 -0700

    Time: 1 ms (77.38%), Space: 44.9 MB (99.88%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,21 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
+       int totalProft = 0;
 
 
-        int totalprofit = 0;
-
-        for(int i =1  ;i<prices.length;i++){
-                if(prices[i] > prices[i-1]){
-
-                totalprofit += prices[i] - prices[i-1];
-                }
-        
-           
-
+        for(int i = 1;i<prices.length;i++){
+            
+            if(prices[i] > prices[i-1]){
+                totalProft+= prices[i]-prices[i-1];
+            }
+            
         }
 
-        return totalprofit;
-        
+        return totalProft;
         
     }
 }
