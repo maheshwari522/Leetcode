e0cdf12cfc9dbd7d3cf83e2088521975e8ea40b5
Maheshwari Bhandare <maheshwaribhandare2205@gmail.com>
2025-11-24 14:27:09 -0800

    Time: 15 ms (77.76%), Space: 47 MB (11.34%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,26 +1,45 @@
-import java.util.Arrays;
-
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] memo = new int[amount + 1];
-        // Fill with a value to indicate not computed yet
-        Arrays.fill(memo, -2);
-        return solve(coins, amount, memo);
-    }
+       if(coins.length == 0 || coins == null) return -1;
+       int n = coins.length;
+       int m = amount;
+       int dp[][] = new int[n+1][m+1];
+       for(int j = 1; j<=m;j++){
+        dp[0][j] = amount+1;
+       }
 
-    private int solve(int[] coins, int rem, int[] memo) {
-        if (rem < 0) return -1;
-        if (rem == 0) return 0;
-        if (memo[rem] != -2) return memo[rem];
-
-        int minCount = Integer.MAX_VALUE;
-        for (int coin : coins) {
-            int res = solve(coins, rem - coin, memo);
-            if (res >= 0 && res < minCount) {
-                minCount = 1 + res;
+       for(int i = 1;i<=n;i++){
+        for(int j = 1;j<=m;j++){
+            if(coins[i-1]>j){
+                dp[i][j] = dp[i-1][j];
+            }else{
+                dp[i][j] = Math.min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
             }
         }
-        memo[rem] = (minCount == Integer.MAX_VALUE) ? -1 : minCount;
-        return memo[rem];
+       }
+       int res= dp[n][m];
+       if(res>=amount+1){
+        return -1;
+       }
+       return res;
     }
+
+    // public int helper(int[] coins, int amount, int index, int coinsused){
+    //     int dp[][] = new int[coins.length+1][amount+1];
+    //     //base case
+    //     if(coins.length == index || amount < 0){
+    //         return -1;
+    //     }
+    //     //valid
+    //     if(amount == 0){
+    //         return coinsused;
+    //     }
+    //     //logic
+    //    int case0 = helper(coins,amount,index+1,coinsused);
+    //    int case1 = helper(coins,amount-coins[index],index,coinsused+1);
+    //    if(case0 == -1) return case1;
+    //    if(case1 == -1) return case0;
+
+    //    return Math.min(case0,case1);
+    // }
 }
