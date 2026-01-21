0d39285dea20d26e489c4ef37632022c8c43f86f
Maheshwari Bhandare <maheshwaribhandare2205@gmail.com>
2026-01-21 13:57:20 -0800

    Time: 42 ms (16.86%), Space: 48 MB (40.08%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,26 +1,37 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-
-        int left = 0;
-        int right = arr.length-1;
-
-        while(left<right && right-left >= k){
-            int j = Math.abs(arr[left] - x);
-            int l = Math.abs(arr[right]-x);
-            if((j<l || (j == l && arr[left]<arr[right]))){
-                right--;
-            }else{
-                left++;
-
-            }
-        }
-
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b)->{
+           int diffA = Math.abs(a - x);
+           int diffB = Math.abs(b - x);
+           if (diffA != diffB) {
+               return diffB - diffA;
+           }
+           return a - b;
+        });
         List<Integer> res = new ArrayList<>();
-
-        for(int i = left;i<=right;i++){
-            res.add(arr[i]);
+        for(int i=0;i<k;i++){
+            int diff = arr[i]-x;
+            
+            maxHeap.add(arr[i]);
+    
         }
-        return res;
+        for(int j = k;j<arr.length;j++){
+            int diff = arr[j]-x;
+            if(Math.abs(maxHeap.peek()-x)>diff){
+            maxHeap.add(arr[j]);
+            }
+            if(maxHeap.size()>k){
+                maxHeap.poll();
+            }
+
+        }
+        while(!maxHeap.isEmpty()){
+            res.add(maxHeap.poll());
+        }
+
+         Collections.sort(res);
+
+         return res;
         
     }
 }
