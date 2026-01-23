978bea4abf44e01cb84872bf969c69f863ef8efc
Maheshwari Bhandare <maheshwaribhandare2205@gmail.com>
2026-01-23 14:15:25 -0800

    Time: 26 ms (48.57%), Space: 45.7 MB (99.98%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,42 +1,36 @@
-class Solution {
+public class Solution {
     public int leastInterval(char[] tasks, int n) {
-
         int[] count = new int[26];
-
-        for(char c:tasks){
-            count[c - 'A']++;
+        for (char task : tasks) {
+            count[task - 'A']++;
         }
 
-       int time = 0;
-
-       PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
-
-        for(int i = 0;i<26;i++){
-            if(count[i]>0){
-                q.add(count[i]);
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int cnt : count) {
+            if (cnt > 0) {
+                maxHeap.add(cnt);
             }
         }
 
-        Queue<int[]> temptask = new LinkedList<>();
-
-        while(!q.isEmpty()||!temptask.isEmpty()){
+        int time = 0;
+        Queue<int[]> q = new LinkedList<>();
+        while (!maxHeap.isEmpty() || !q.isEmpty()) {
             time++;
-            if(q.isEmpty()){
-                 time = temptask.peek()[1];
-
-            }else{
-                int currentfreq = q.poll()-1;
-                if(currentfreq>0){
-                temptask.add(new int[]{currentfreq,time+n});
+            
+            if (maxHeap.isEmpty()) {
+                time = q.peek()[1];
+            } else {
+                int cnt = maxHeap.poll() - 1;
+                if (cnt > 0) {
+                    q.add(new int[]{cnt, time + n});
                 }
             }
 
-            if(!temptask.isEmpty() && temptask.peek()[1] == time){
-                q.add(temptask.poll()[0]);
+            if (!q.isEmpty() && q.peek()[1] == time) {
+                maxHeap.add(q.poll()[0]);
             }
         }
-
-        return time;
         
+        return time;
     }
 }
